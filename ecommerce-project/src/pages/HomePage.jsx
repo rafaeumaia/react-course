98e2995689a743039e86f537bc75aa73d6290336
Rafael Maia <rafaeumaia@users.noreply.github.com>
2025-12-04 12:05:55 -0300

    refactor: use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,16 +1,13 @@
+import axios from "axios";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products.js";
 import "./HomePage.css";
 import Checkmark from "../assets/images/icons/checkmark.png";
 
 function HomePage() {
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
 
   return (
     <>
